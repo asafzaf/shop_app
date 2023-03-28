46bd804354997ada44af007d2e8679763653022d
asafzaf <asaf.zafrir@gmail.com>
2023-03-28 11:58:12 +0300

    Start a new project, Added first Screen, Class and Item.

---
+++ lib/model/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  Product({
+    this.id,
+    this.title,
+    this.description,
+    this.price,
+    this.imageUrl,
+  });
+}
