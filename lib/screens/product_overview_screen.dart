95f24d236f16cb816bcdaf1055583aea750b4009
asafzaf <asaf.zafrir@gmail.com>
2023-03-30 21:22:57 +0300

    change import because of changing path to product class

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
 
-import '../models/product.dart';
+import '../providers/product.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
