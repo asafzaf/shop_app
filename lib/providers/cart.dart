fab3249d0b2e595ecdb5e0320c5e5fa91a793a19
asafzaf <asaf.zafrir@gmail.com>
2023-04-01 00:11:34 +0300

    Added "cart" class with add method and getter.

---
+++ lib/providers/cart.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (exsistingCartItem) => CartItem(
+                id: exsistingCartItem.id,
+                title: exsistingCartItem.title,
+                price: exsistingCartItem.price,
+                quantity: exsistingCartItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
+    }
+  }
+}
