ef0fb88bbba26980682e69150cdac8cf4713ca53
asafzaf <asaf.zafrir@gmail.com>
2023-04-01 20:54:52 +0300

    Added product detail screen. Added futures to cart screen. Added cart functions (remove and clear),

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
+
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart-screen';
@@ -10,41 +13,60 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final cart = Provider.of<Cart>(context);
     return Scaffold(
-        appBar: AppBar(title: Text('text')),
-        body: Column(
-          children: [
-            Card(
-              margin: EdgeInsets.all(15),
-              child: Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('total', style: TextStyle(fontSize: 20)),
-                      Spacer(),
-                      // SizedBox(width: 10),
-                      Chip(
-                        label: Text(
-                          '\$${cart.totalAmount}',
-                          style: TextStyle(
-                              color: Theme.of(context)
-                                  .primaryTextTheme
-                                  .titleMedium
-                                  .color),
-                        ),
-                        backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(title: Text('Your Cart')),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('total', style: TextStyle(fontSize: 20)),
+                    Spacer(),
+                    // SizedBox(width: 10),
+                    Chip(
+                      label: Text(
+                        '\$${cart.totalAmount.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            color: Theme.of(context)
+                                .primaryTextTheme
+                                .titleMedium
+                                .color),
                       ),
-                      MaterialButton(
-                        onPressed: () {},
-                        child: Text(
-                          'ORDER NOW',
-                        ),
-                        textColor: Theme.of(context).primaryColor,
-                      )
-                    ],
-                  )),
+                      backgroundColor: Theme.of(context).primaryColor,
+                    ),
+                    MaterialButton(
+                      onPressed: () {
+                        Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(),
+                          cart.totalAmount,
+                        );
+                        cart.clear();
+                      },
+                      textColor: Theme.of(context).primaryColor,
+                      child: const Text(
+                        'ORDER NOW',
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+              child: ListView.builder(
+            itemCount: cart.items.length,
+            itemBuilder: (ctx, i) => CartItem(
+              cart.items.values.toList()[i].id,
+              cart.items.keys.toList()[i],
+              cart.items.values.toList()[i].price,
+              cart.items.values.toList()[i].quantity,
+              cart.items.values.toList()[i].title,
             ),
-          ],
-        ));
+          )),
+        ],
+      ),
+    );
   }
 }
