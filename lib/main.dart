de12b4ed25fb9483c1710984bfbff99a870402b3
asafzaf <asaf.zafrir@gmail.com>
2023-04-01 20:53:12 +0300

    Added appDrawer to app. Added orders screen and order function. Added intl libary for dates formats. Added dart:math for math functions.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:provider/provider.dart';
 
 import './providers/cart.dart';
 import './providers/products.dart';
+import './providers/orders.dart';
 
 import './screens/product_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
+import './screens/orders_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,6 +26,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: ((ctx) => Cart()), // for a list or maps (extendend items).
         ),
+        ChangeNotifierProvider(
+          create: ((ctx) => Orders()), // for a list or maps (extendend items).
+        ),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
@@ -36,6 +41,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (context) => CartScreen(),
+          OrdersScreen.routeName: (context) => OrdersScreen(),
         },
       ),
     );
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../widgets/app_drawer.dart';
 
 import '../screens/cart_screen.dart';
 import '../widgets/products_grid.dart';
@@ -60,6 +61,7 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
