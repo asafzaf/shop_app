3a151d584cc5bd0efff9950974ca019f6bb1b253
asafzaf <asaf.zafrir@gmail.com>
2023-04-02 10:28:45 +0300

    Added Snackbar into adding to cart Action.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -59,6 +59,19 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text(
+                  'Added item to cart!',
+                ),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
             },
             color: Theme.of(context).accentColor,
           ),
