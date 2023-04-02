5d2a5b96ce2924402899aab7a2a85c493f7bc6ab
asafzaf <asaf.zafrir@gmail.com>
2023-04-02 14:13:47 +0300

    Adding a form for add/edit product.
    created some inputs to the form.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -44,6 +45,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
           UserProductsScreen.routeName: (context) => UserProductsScreen(),
+          EditProductScreen.routeName: (context) => EditProductScreen(),
         },
       ),
     );
