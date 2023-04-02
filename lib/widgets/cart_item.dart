ae392dbed7d8750b32a35c4ee3c4fd97a6fe2c94
asafzaf <asaf.zafrir@gmail.com>
2023-04-02 10:39:17 +0300

    Added delete dialog for Cart items.

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -37,6 +37,27 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('Are you sure?'),
+                  content:
+                      Text('Do you want to remove the item from the cart?'),
+                  actions: [
+                    MaterialButton(
+                        child: Text('No'),
+                        onPressed: () {
+                          Navigator.of(ctx).pop(false);
+                        }),
+                    MaterialButton(
+                        child: Text('Yes'),
+                        onPressed: () {
+                          Navigator.of(context).pop(true);
+                        }),
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
